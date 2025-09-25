f22d8b261d6bc77705653b2e84d224125f486a99
samucaasantos <sads200528@gmail.com>
2025-09-25 00:41:25 -0300

    removal of unnecessary modules imports

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,7 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 
 public class Program {
