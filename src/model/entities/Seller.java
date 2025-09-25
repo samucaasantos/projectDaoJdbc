786931228a1cf0260d1c0892ed549de055ca1619
samucaasantos <sads200528@gmail.com>
2025-09-25 00:08:04 -0300

    findById implementation and seller class refactoration

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -10,19 +10,19 @@ public class Seller implements Serializable {
     private String name;
     private String email;
     private Date birthDate;
-    private Double salary;
+    private Double baseSalary;
     private Department department;
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Seller() {}
 
-    public Seller(Integer id, String name, String email, Date birthDate, Double salary, Department department) {
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.birthDate = birthDate;
-        this.salary = salary;
+        this.baseSalary = baseSalary;
         this.department = department;
     }
 
@@ -58,12 +58,12 @@ public class Seller implements Serializable {
         this.birthDate = birthDate;
     }
 
-    public Double getSalary() {
-        return salary;
+    public Double getBaseSalary() {
+        return baseSalary;
     }
 
-    public void setSalary(Double salary) {
-        this.salary = salary;
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
     }
 
     public Department getDepartment() {
@@ -94,7 +94,7 @@ public class Seller implements Serializable {
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", birthDate=" + birthDate +
-                ", salary=" + salary +
+                ", baseSalary=" + baseSalary +
                 ", department=" + department +
                 '}';
     }
